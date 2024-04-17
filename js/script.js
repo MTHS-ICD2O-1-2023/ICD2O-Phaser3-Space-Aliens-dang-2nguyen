555e0ef2636428ac621d4c35bdebd843989bbec6
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-17 17:45:47 +0000

    e56u47u4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+const config = {
+  type: Phaser.AUTO,
+  width: 2300,
+  height: 1080,
+  backgroundcolor: 0x5f6e7a,
+  physic: {
+    default: "arcade",
+    arcade:{
+      debug: true,
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autocenter: Phaser.Scale.CENTER_BOTH,
+  }
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
