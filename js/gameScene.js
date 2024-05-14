a234f1b04ec4de3fd091d2a40b5152ae12b4e568
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-05-14 17:45:42 +0000

    sfsdfsdf

--- js/gameScene.js
+++ js/gameScene.js
@@ -6,6 +6,16 @@
 // This file contains the JS functions for index.html
 
 class GameScene extends Phaser.Scene {
+  createAlien() {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1
+    let alienXVelocity = Math.floor(Math.random()* 50) +1
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
+    this.alienGroup.add(anAlien)
+  }
+
   constructor() {
     super({ key: "gameScene" })
     this.background = null
@@ -24,6 +34,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground","./asset/starBackground.png")
     this.load.image("ship","./asset/spaceShip.png")
     this.load.image("missile", "asset/missile.png")
+    this.load.image("alien", "asset/alien.png")
     this.load.audio('laser',"asset/laser1.wav")
   }
   /**@param {object} data */
@@ -34,6 +45,9 @@ class GameScene extends Phaser.Scene {
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100 , "ship")
 
     this.missileGroup = this.physics.add.group()
+
+    this.alienGroup = this.add.group()
+    this.createAlien()
   }
   /**
     *@param {number} delta
